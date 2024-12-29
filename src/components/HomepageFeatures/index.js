91e3bbba24b42437b3728287c2e1370988f2058d
Tillman Mosley III <tmosleyiii@gmail.com>
2024-12-29 07:40:50 -0500

    update index

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,39 +4,18 @@ import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 
 const FeatureList = [
+
   {
-    title: "Easy to Use",
-    link: "http://www.withobsrvr.com",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: "Quick Start",
+    title: "OBSRVR Intro",
     link: "/docs/intro",
     Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Learn about OBSRVR and how to get started.
       </>
     ),
   },
-  {
-    title: "Powered by React",
-    link: "http://www.withobsrvr.com",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+
 ];
 
 function Feature({ Svg, title, link, description }) {
